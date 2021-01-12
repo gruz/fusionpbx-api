7017c59b4f5c1400b5e3e55403396cf3a9bbee3d
futsurdmitriy <futsurdmitriy@gmail.com>
2021-01-12 11:13:25 +0100

    Basic implementation of type validation rules generation  based on db column type. Extension update request implementaion.

--- laravel-api/app/Http/ApiRequest.php
+++ laravel-api/app/Http/ApiRequest.php
@@ -2,13 +2,72 @@
 
 namespace App\Http;
 
+use App\Database\Eloquent\Model;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
+use \Illuminate\Support\Str;
 
 abstract class ApiRequest extends FormRequest
 {
+
+    public $mapping = [
+        "decimal" => "numeric",
+        "text" => "string",
+        "guid" => "uuid",
+    ];
+
+    /**
+     * 
+     *  
+     * */    
+    protected function buildDefaultRules(Model $model) 
+    {
+        /**
+         * @var Illuminate\Database\Eloquent\Model $model
+         */
+        $modelName = Str::lower(
+                     class_basename(
+                     get_class($model)));
+        $columns = $model->getTableColumnsInfo(true);
+        $rules = [];
+
+        /**
+         * @var Doctrine\DBAL\Schema\Column $column
+         */
+        foreach ($columns as $column) {
+            $columnName = $column->getName();
+            
+            /**
+             * @var Doctrine\DBAL\Types\Type $columnType
+             */
+            $columnType = $column->getType();
+            $columnTypeName = $columnType->getName();
+            $columnIsNotNullType = $column->getNotnull();
+
+            // We should append column name with prefix if it`s nested 
+            // or if it`s same as model name
+            // if ($columnName == $modelName) {
+            //      $columnName =  $modelName . '.' . $columnName;
+            // }
+
+            $prefixedColumnName = $modelName . '.' . $columnName;
+            $rules[$prefixedColumnName] = $columnTypeName;
+            $rule[] = $this->mapping[$columnTypeName] ?
+                      $this->mapping[$columnTypeName] :
+                      $columnTypeName;
+
+            // Check if field is required or can be null.
+            $rule[] = $columnIsNotNullType ? 'required' : 'nullable';
+
+            $rules[$prefixedColumnName] = implode('|', $rule);
+            unset($rule);
+        }
+
+        return $rules;
+    }
+
     protected function failedValidation(Validator $validator)
     {
         throw new UnprocessableEntityHttpException($validator->errors()->toJson());
--- laravel-api/app/Traits/FusionPBXTableModel.php
+++ laravel-api/app/Traits/FusionPBXTableModel.php
@@ -2,6 +2,8 @@
 namespace App\Traits;
 
 use App\Traits\Uuids;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 trait FusionPBXTableModel
 {
@@ -11,6 +13,8 @@ trait FusionPBXTableModel
     public static $staticHidden;
     public static $staticMakeVisible;
     public static $staticVisible;
+    // protected static $_columns_info = NULL;
+    // protected static $_nullable_fields = NULL;
 
     public function __construct($attributes = array())
     {
@@ -45,6 +49,69 @@ trait FusionPBXTableModel
         $this->keyType = 'uuid';
 
     }
+
+    /**
+     * Gets column names from table associated with current model
+     * 
+     * @return array 
+     */
+    public function getTableColumnNames() 
+    {
+        $tableColumns = $this->getConnection()
+                             ->getSchemaBuilder()
+                             ->getColumnListing($this->getTable());
+        
+        $fillableProps = $this->getFillable();
+        $result = array_intersect($tableColumns, $fillableProps);
+
+        return $result;
+    }
+
+    /**
+     * Gets columns info from table associated with current model 
+     * [name => [info]] array.
+     * 
+     * @return array
+     */
+    public function getTableColumnsInfo()
+    {
+        $columns_info = [];
+        $column_names = $this->getTableColumnNames();
+        foreach ($column_names as $column_name) {
+            $info = $this->getConnection()
+                         ->getDoctrineColumn($this->getTable(), $column_name);
+            $columns_info[$column_name] = $info;
+        }
+
+        return $columns_info;
+    }
+
+    // public function is_nullable(string $field_name)
+    // {
+    //     if (is_null(static::$_columns_info) ){
+    //         static::$_columns_info = $this->getTableColumnsInfo(true);
+    //         // static::$_columns_info = DB::table($db_name.'.columns')
+    //         //     ->where('table_name',$this->getTable())
+    //         //     ->get();
+    //     }
+    //     $colmns = static::$_columns_info;
+    //     if (is_null(static::$_nullable_fields) ){
+    //         foreach ($colmns as $fieldName => $fieldData) {}
+    //         static::$_nullable_fields = array_map( 
+    //                 function ($fld){return $fld['notnull'];},
+    //                 $colmns
+    //         );
+    //     }
+
+    //     return in_array($field_name, static::$_nullable_fields);
+    // }
+
+    // public function getDbFieldType($field)
+    // {
+    //     $fieldType = DB::getSchemaBuilder()->getColumnType($this->getTable(), $field);
+        
+    //     return $fieldType;
+    // }
 
     public function __destruct()
     {
