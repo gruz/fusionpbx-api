b8c84f5962acd6c070565409985eaccf94718028
Gruz <arygroup@gmail.com>
2021-01-18 02:57:27 +0200

    Cleanup

--- laravel-api/infrastructure/Http/Controllers/FrontController.php
+++ laravel-api/infrastructure/Http/Controllers/FrontController.php
@@ -7,28 +7,28 @@ use Illuminate\Routing\Controller as BaseController;
 
 class FrontController extends BaseController
 {
-
-    public function test() {
+    public function test()
+    {
+        if (!config('app.debug')) {
+            return;
+        }
         \DB::enableQueryLog(); // Enable query log
         $items = \Api\Extension\Models\Extension::with('extension_users.permissions')
             ->where('extension_uuid', '63045580-4ce3-11eb-b21b-47744eb6524b')
             ->get()
             ->toArray();
-        \dd($items,\DB::getQueryLog());
+        \dd($items, \DB::getQueryLog());
 
         phpinfo();
         try {
 
             \DB::enableQueryLog(); // Enable query log
-            $user = User::
-            with('groups')
-            ->get()->toArray();
-    
-            \dd($user,\DB::getQueryLog());
+            $user = User::with('groups')
+                ->get()->toArray();
 
+            \dd($user, \DB::getQueryLog());
         } catch (\Exception $e) {
             dd($e);
-            
         }
     }
 }
