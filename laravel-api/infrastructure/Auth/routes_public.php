0d2bcb6e3d8b4bc72a3567e28900db5307a73845
Gruz <arygroup@gmail.com>
2021-02-11 05:17:54 +0200

    Adds ReDoc

--- laravel-api/infrastructure/Auth/routes_public.php
+++ laravel-api/infrastructure/Auth/routes_public.php
@@ -1,4 +1,4 @@
 <?php
 
-$router->post('/login', 'LoginController@login');
+$router->post('/login', ['uses' => 'LoginController@login',  'as' => 'login']);
 $router->post('/login/refresh', 'LoginController@refresh');
--- laravel-api/infrastructure/Http/routes_public.php
+++ laravel-api/infrastructure/Http/routes_public.php
@@ -1,3 +1,7 @@
 <?php
 
 $router->get('/test', [ \Infrastructure\Http\Controllers\FrontController::class, 'test' ], ['middleware' => 'web']);
+
+$router->get('/api/redoc', function(){
+    return view('documenation.index');
+});
