37b469568564654ffc7662b37ead46bbd9863cdf
futsurdmitriy <futsurdmitriy@gmail.com>
2021-03-09 16:40:16 +0100

    Adding Model way to set validation rules when password reset were requested.

--- laravel-api/api/User/Requests/UserForgotPasswordRequest.php
+++ laravel-api/api/User/Requests/UserForgotPasswordRequest.php
@@ -3,6 +3,8 @@
 namespace Api\User\Requests;
 
 use Infrastructure\Http\ApiRequest;
+use Api\User\Models\User;
+use Api\Domain\Models\Domain;
 
 class UserForgotPasswordRequest extends ApiRequest
 {
@@ -14,8 +16,8 @@ class UserForgotPasswordRequest extends ApiRequest
     public function rules()
     {
         return [
-            'domain_name'    => 'required|exists:v_domains,domain_name', 
-            'user_email' => 'required|email|exists:v_users,user_email'
+            'domain_name' => 'required|exists:' . Domain::class . ',domain_name', 
+            'user_email' => 'required|email|exists:' . User::class . ',user_email'
         ];
     }
    
