8de0d5c743c95440d5b3eb3b62f46cdd296fd172
Gruz <arygroup@gmail.com>
2021-03-21 22:03:38 +0200

    WIP

--- laravel-api/infrastructure/Version.php
+++ laravel-api/infrastructure/Version.php
@@ -7,6 +7,6 @@ class Version
     public static function getGitTag()
     {
         $versionFile = base_path('version.txt');
-        return file_exists($versionFile) ? file_get_contents($versionFile) : exec('git describe --tags');
+        return file_exists($versionFile) ? trim(file_get_contents($versionFile)) : exec('git describe --tags');
     }
 }
