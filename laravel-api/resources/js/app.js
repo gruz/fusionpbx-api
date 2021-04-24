4ba4b50cacefbb7508dec3b596c425fc3bf55f05
Gruz <arygroup@gmail.com>
2021-04-24 09:49:32 +0300

    VUE tries

---
+++ laravel-api/resources/js/app.js
@@ -0,0 +1,10 @@
+require('./bootstrap');
+
+import Vue from 'vue';
+
+// import App from './vue/app';
+
+const app = new Vue({
+    el: '#app',
+    components: { App }
+});
---
+++ laravel-api/resources/js/vue/app.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        Hellow
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+// import { defineComponent } from '@vue/composition-api'
+
+// export default defineComponent({
+//     setup() {
+        
+//     },
+// })
+</script>
