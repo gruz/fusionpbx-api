8dd29559c0305729f73d3d70441ee9c136286f47
Gruz <arygroup@gmail.com>
2021-05-10 06:49:40 +0300

    Changes routes for API

--- laravel-api/infrastructure/Auth/routes_public.php
+++ laravel-api/infrastructure/Auth/routes_public.php
@@ -1,4 +1,4 @@
 <?php
 
-$router->post('/login', ['uses' => 'LoginController@login',  'as' => 'login']);
-$router->post('/login/refresh', 'LoginController@refresh');
+$router->post('/api/login', ['uses' => 'LoginController@login',  'as' => 'login']);
+$router->post('/api/login/refresh', 'LoginController@refresh');
