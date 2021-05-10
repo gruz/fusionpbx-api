5946f38d09b4d92c9b7508aab8a6d32476f4b4fd
Gruz <arygroup@gmail.com>
2021-05-10 08:32:45 +0300

    Updates user actions listeners

--- laravel-api/api/User/Listeners/UserWasActivatedListener.php
+++ laravel-api/api/User/Listeners/UserWasActivatedListener.php
@@ -5,6 +5,7 @@ namespace Api\User\Listeners;
 use Illuminate\Support\Facades\Notification;
 
 use Api\User\Notifications\UserWasActivatedSelfNotification;
+use Api\User\Notifications\UserWasActivatedDomainAdminNotification;
 
 class UserWasActivatedListener
 {
@@ -13,5 +14,13 @@ class UserWasActivatedListener
         if ($event->sendNotification) {
             Notification::send($event->user, new UserWasActivatedSelfNotification($event->user));
         }
+
+        $admins = $event->user->getDomainAdmins();
+        Notification::send($admins, new UserWasActivatedDomainAdminNotification($event->user));
+
+        $mainAdminEmail = config('mail.from.address');
+
+        Notification::route('mail', $mainAdminEmail)
+            ->notify(new UserWasActivatedDomainAdminNotification($event->user));
     }
 }
--- laravel-api/api/User/Listeners/UserWasCreatedListener.php
+++ laravel-api/api/User/Listeners/UserWasCreatedListener.php
@@ -2,42 +2,22 @@
 
 namespace Api\User\Listeners;
 
-use Api\User\Mail\UserNew;
 use Illuminate\Support\Arr;
-use Api\Domain\Mail\DomainNew;
-use Infrastructure\Notification\Notification;
-use Api\User\Notifications\UserWasCreatedSendVeirfyLinkNotification;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+
 
 class UserWasCreatedListener
 {
     public function handle($event)
     {
         // Send veirification links to users
-        $userActivated = $event->user->getAttribute('user_enabled');
-        if (true !== $userActivated) {
+        if ($event->user instanceof MustVerifyEmail && ! $event->user->hasVerifiedEmail()) {
+
             $excludeNotificationEmails = Arr::get($event->options, 'excludeNotification', []);
             if (!in_array($event->user->user_email, $excludeNotificationEmails)) {
-                $notification = new UserWasCreatedSendVeirfyLinkNotification($event->user);
-                Notification::send($event->user, $notification);
-            }
-        }
-
-        return;
-        // Notify domain admins on new user registration
-
-        $admins = $event->user->getDomainAdmins()->get();
-
-        foreach ($admins as $k => $admin) {
-            $emails = [];
-
-            foreach ($admin->emails as $k => $email) {
-                $emails[] = $email->email_address;
-            }
-
-            if ($event->user->user_uuid !== $admin->user_uuid) {
-                \Mail::to($emails)->send(new UserNew($event->user));
-            } else {
-                \Mail::to($emails)->send(new DomainNew($event->user));
+                $event->user->sendEmailVerificationNotification();
+                // $notification = new UserWasCreatedSendVeirfyLinkNotification($event->user);
+                // Notification::send($event->user, $notification);
             }
         }
     }
