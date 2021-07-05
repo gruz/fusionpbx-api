097e0d1dc51992e9f1283206ef08ce7b23c9ca06
Gruz <arygroup@gmail.com>
2021-07-05 14:48:27 +0300

    Adds lowercase middleware

---
+++ app/Http/Middleware/LowercaseRequestParams.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Foundation\Http\Middleware\TransformsRequest;
+
+class LowercaseRequestParams extends TransformsRequest
+{
+    /**
+     * Transform the given value.
+     *
+     * @param  string  $key
+     * @param  mixed  $value
+     * @return mixed
+     */
+    protected function transform($key, $value)
+    {
+        $field_names = [
+            'domain_name',
+            'user_email',
+            'username',
+        ];
+        return in_array($key, $field_names) ? \strtolower($value) : $value;
+    }
+}
