f58cfb2c72d6497a03a854a6755e0b7fcc1cf315
Gruz <arygroup@gmail.com>
2021-08-30 17:14:09 +0300

    Adds default mail configurations

--- config/mail.php
+++ config/mail.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'contact_email'=> env('MAIL_CONTACT', 'info@example.com'),
-    'error_email'=> env('MAIL_ERROR', 'info@example.com'),
+    'contact_email'=> env('MAIL_CONTACT', env('MAIL_FROM_ADDRESS','info@example.com')),
+    'error_email'=> env('MAIL_ERROR', env('MAIL_FROM_ADDRESS','info@example.com')),
     /** Notifications about new registrations */
-    'new_registration_email'=> env('MAIL_NEW_REGISTRATION', 'info@example.com'),
+    'new_registration_email'=> env('MAIL_NEW_REGISTRATION', env('MAIL_FROM_ADDRESS','info@example.com')),
 ];
