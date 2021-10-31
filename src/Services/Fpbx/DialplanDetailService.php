137b868abfc9ea34fa4ed0b9ecf77f1b18469949
Gruz <arygroup@gmail.com>
2021-10-31 11:27:47 +0200

    Fixes not getting enabled by null value dialplans

--- src/Services/Fpbx/DialplanDetailService.php
+++ src/Services/Fpbx/DialplanDetailService.php
@@ -10,9 +10,13 @@ class DialplanDetailService extends AbstractService
             'dialplan_detail_tag' => 'condition',
             'dialplan_detail_type' => 'destination_number',
             'dialplan_detail_data' => $destination_number,
-            'dialplan_detail_enabled' => true,
+            // 'dialplan_detail_enabled' => true,
         ], ['includes' => ['transferExtension']])->first();
 
+        // In this case dialplan is treated as enabled in FPBX when null value is set
+        if (!in_array($dialplanDetails->dialplan_detail_enabled, [null, true, 'true'])) {
+            return false;
+        }
 
         if ($dialplanDetails) {
             $transferExtension = optional($dialplanDetails->transferExtension)->dialplan_detail_data;
